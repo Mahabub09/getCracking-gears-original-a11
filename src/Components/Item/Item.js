e89e39baa95f013fb6d45b007408c1299a4634f5
mahbub09 <mahabub895alam@gmail.com>
2022-05-05 02:08:56 +0600

    data added

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Item = ({ item }) => {
-    const { id, city, img, title, price, duration } = item
+    const { id, name, img, supplier_name, price, short_desciption, quantity } = item
     const navigate = useNavigate();
 
-    const navigateToServiceDetail = id => {
+    const navigateToItemDetail = id => {
         navigate(`/item/${id}`);
     }
     return (
@@ -14,16 +14,16 @@ const Item = ({ item }) => {
             <div className='grid-cols-3 container shadow-lg p-3 mb-5 bg-body rounded'>
                 <img className='w-100' src={img} alt="" />
                 <div className=' d-flex justify-content-between align-items-center px-3 my-2'>
-                    <div><h4><i>{city}</i></h4>
-                        <h6><b>{title}</b></h6>
+                    <div><h4><i>{name}</i></h4>
+                        <h6><b>{supplier_name}</b></h6>
                     </div>
                     <div>
-                        <h5>{duration}</h5>
+                        <h5>{quantity}</h5>
                         <p><b>Cost: {price}</b></p>
                     </div>
                 </div>
 
-                <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary align-items-center' >Book: {city}</button>
+                <button onClick={() => navigateToItemDetail(id)} className='btn btn-primary align-items-center' >GET: {name}</button>
             </div>
         </div>
     );
